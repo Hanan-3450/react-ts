0258ff19cdb0f019bcb904ee1318dd7da25c691b
Hanan <hanumd6127@gmail.com>
2025-07-11 08:58:34 -0700

    finalize todo fetching with loader and component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { useState, useEffect } from "react";
 import "./App.css";
+import Loader from "./components/loader";
+import Todo from "./components/Todo";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  const [todos, setTodos] = useState([]);
+  const [error, setError] = useState([]);
+
+  interface Todo {
+    title: string;
+    id: number;
+    completed: boolean;
+  }
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((res) => setTodos(res.slice(0, 10)))
+      .catch((err) => setError(err));
+  }, []);
+
+  return (
+    <div className="App">
+      {todos.length > 0 ? todos.map((todo: Todo) => <Todo />) : <Loader />}
+    </div>
+  );
 }
 
 export default App;
